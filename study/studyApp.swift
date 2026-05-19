b902b486c2de9d67a782b2e301a101e6c47cb129
joyeajin <dpwls31200@gmail.com>
2026-05-19 10:44:25 +0900

    Initial Commit

---
+++ study/studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyApp.swift
+//  study
+//
+//  Created by 예진 on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ studyTests/studyTests.swift
@@ -0,0 +1,16 @@
+//
+//  studyTests.swift
+//  studyTests
+//
+//  Created by 예진 on 5/19/26.
+//
+
+import Testing
+
+struct studyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
